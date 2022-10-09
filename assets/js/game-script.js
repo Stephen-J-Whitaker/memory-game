2f3d3b960d9c5c1be21def1d1fa1f060fee8c204
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2022-10-09 17:36:28 +0000

    Start adding functions to game script

--- assets/js/game-script.js
+++ assets/js/game-script.js
@@ -1,7 +1,32 @@
 // When DOM fully loaded add listeners to handle user interface events
 
 document.addEventListener("DOMContentLoaded", function() {
+
+    // Triggers dropdown of instructions on click
     document.getElementById("instructions-button").addEventListener("click", function() {
         document.getElementById("instructions").classList.toggle("hide-instructions");
     })
+
+    // Start the game on click of start button
+    document.getElementById("start").addEventListener("click", gameStart());
 })
+
+/**
+ * Checks if muted and creates a different tone depending on the button pressed
+ */
+function tone(colourButton) {
+
+}
+
+/**
+ * Makes the button look likes its being pressed by hiding it to show a smaller
+ * version of the button underneath
+ */
+
+function buttonPress(colourButton) {
+
+}
+
+function checkScore() {
+
+}
